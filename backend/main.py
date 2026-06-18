23911602ab6befbfe1ab66af460c915f5b6e454a
Igor Araujo <igorlaraujo@outlook.com>
2026-06-18 14:29:27 -0300

    Atualiza CORS para uso local sem IDE

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:63342",
         "http://127.0.0.1:63342",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
     ],
     allow_credentials=True,
     allow_methods=["*"],
